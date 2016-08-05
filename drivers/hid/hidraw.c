80c1d00b54f9dc77fec1188564918dd8c0fe6205
Ankita Garg <ankitag@nvidia.com>
2016-08-05 16:27:43 -0700

    HID: hidraw: Add spinlock in struct hidraw to protect list
    It is unsafe to call list_for_each_entry in hidraw_report_event to
    traverse each hidraw_list node without a lock protection, the list
    could be modified if someone calls hidraw_release and list_del to
    remove itself from the list, this can cause hidraw_report_event
    to touch a deleted list struct and panic.
    
    To prevent this, introduce a spinlock in struct hidraw to protect
    list from concurrent access.
    
    Commit-id of upstream change: 277fe44dd862412ee034470ad1c13a79d24e533b
    
    Bug 200216971
    
    Change-Id: Ib144abe4851e2cae12e7533bee763e8d9a97d5d4
    Signed-off-by: Yonghua Zheng <younghua.zheng@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Ankita Garg <ankitag@nvidia.com>
    Reviewed-on: http://git-master/r/1198707
    Reviewed-by: Raymond Poudrier <rapoudrier@nvidia.com>
    Reviewed-by: David Dastous St Hilaire <ddastoussthi@nvidia.com>

--- drivers/hid/hidraw.c
+++ drivers/hid/hidraw.c
@@ -7,7 +7,7 @@
  * use a transport-specific userspace libhid/libusb libraries.
  *
  *  Copyright (c) 2007 Jiri Kosina
- * Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2016, NVIDIA CORPORATION.  All rights reserved.
  */
 
 /*
@@ -259,6 +259,7 @@ static int hidraw_open(struct inode *inode, struct file *file)
 	unsigned int minor = iminor(inode);
 	struct hidraw *dev;
 	struct hidraw_list *list;
+	unsigned long flags;
 	int err = 0;
 
 	if (!(list = kzalloc(sizeof(struct hidraw_list), GFP_KERNEL))) {
@@ -272,11 +273,6 @@ static int hidraw_open(struct inode *inode, struct file *file)
 		goto out_unlock;
 	}
 
-	list->hidraw = hidraw_table[minor];
-	mutex_init(&list->read_mutex);
-	list_add_tail(&list->node, &hidraw_table[minor]->list);
-	file->private_data = list;
-
 	dev = hidraw_table[minor];
 	if (!dev->open++) {
 		err = hid_hw_power(dev->hid, PM_HINT_FULLON);
@@ -289,9 +285,16 @@ static int hidraw_open(struct inode *inode, struct file *file)
 		if (err < 0) {
 			hid_hw_power(dev->hid, PM_HINT_NORMAL);
 			dev->open--;
+			goto out_unlock;
 		}
 	}
 
+	list->hidraw = hidraw_table[minor];
+	mutex_init(&list->read_mutex);
+	spin_lock_irqsave(&hidraw_table[minor]->list_lock, flags);
+	list_add_tail(&list->node, &hidraw_table[minor]->list);
+	spin_unlock_irqrestore(&hidraw_table[minor]->list_lock, flags);
+	file->private_data = list;
 out_unlock:
 	mutex_unlock(&minors_lock);
 out:
@@ -338,15 +341,19 @@ static int hidraw_release(struct inode * inode, struct file * file)
 {
 	unsigned int minor = iminor(inode);
 	struct hidraw_list *list = file->private_data;
+	unsigned long flags;
 	int i;
 
 	mutex_lock(&minors_lock);
+
+	spin_lock_irqsave(&hidraw_table[minor]->list_lock, flags);
 
 	for (i = 0; i < HIDRAW_BUFFER_SIZE; i++) {
 		kfree(list->buffer[i].value);
 	}
 
 	list_del(&list->node);
+	spin_unlock_irqrestore(&hidraw_table[minor]->list_lock, flags);
 	kfree(list);
 
 	drop_ref(hidraw_table[minor], 0);
@@ -475,7 +482,9 @@ int hidraw_report_event(struct hid_device *hid, u8 *data, int len)
 	struct hidraw *dev = hid->hidraw;
 	struct hidraw_list *list;
 	int ret = 0;
+	unsigned long flags;
 
+	spin_lock_irqsave(&dev->list_lock, flags);
 	list_for_each_entry(list, &dev->list, node) {
 		int new_head = (list->head + 1) & (HIDRAW_BUFFER_SIZE - 1);
 
@@ -491,6 +500,7 @@ int hidraw_report_event(struct hid_device *hid, u8 *data, int len)
 		list->head = new_head;
 		kill_fasync(&list->fasync, SIGIO, POLL_IN);
 	}
+	spin_unlock_irqrestore(&dev->list_lock, flags);
 
 	wake_up_interruptible(&dev->wait);
 	return ret;
@@ -539,6 +549,7 @@ int hidraw_connect(struct hid_device *hid)
 
 	mutex_unlock(&minors_lock);
 	init_waitqueue_head(&dev->wait);
+	spin_lock_init(&dev->list_lock);
 	INIT_LIST_HEAD(&dev->list);
 
 	dev->hid = hid;
