b3f1284b401c9c0a836383a8838c791d74425166
Ian Chang <ianc@nvidia.com>
2016-11-07 16:14:16 +0800

    Revert "ext4: use old interface for ext4_readdir()"
    This reverts commit b6b8b43596251f7f1e8379665ac2bcc4aa1c831c.
    
    The change we revert will cause file system iterate call back
    function broken. So reverting that.
    
    bug 200205692
    
    Change-Id: I95cf767789c3c93ff772d55ddc53669bb7eadd81
    Signed-off-by: Ian Chang <ianc@nvidia.com>
    Reviewed-on: http://git-master/r/1249510
    Reviewed-by: Robert Shih <rshih@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- fs/ext4/dir.c
+++ fs/ext4/dir.c
@@ -26,7 +26,6 @@
 #include <linux/buffer_head.h>
 #include <linux/slab.h>
 #include <linux/rbtree.h>
-#include <linux/fsnotify.h>
 #include "ext4.h"
 #include "xattr.h"
 
@@ -103,7 +102,7 @@ int __ext4_check_dir_entry(const char *function, unsigned int line,
 	return 1;
 }
 
-static int ext4_readdir2(struct file *file, struct dir_context *ctx)
+static int ext4_readdir(struct file *file, struct dir_context *ctx)
 {
 	unsigned int offset;
 	int i;
@@ -284,18 +283,6 @@ errout:
 #endif
 	brelse(bh);
 	return err;
-}
-
-static int ext4_readdir(struct file *file,
-			void *dirent, filldir_t filldir)
-{
-	struct dir_context ctx = { filldir, dirent, file->f_pos };
-	int ret;
-
-	ret = ext4_readdir2(file, &ctx);
-	file->f_pos = ctx.pos;
-	fsnotify_access(file);
-	return ret;
 }
 
 static inline int is_32bit_api(void)
@@ -660,7 +647,7 @@ int ext4_check_all_de(struct inode *dir, struct buffer_head *bh, void *buf,
 const struct file_operations ext4_dir_operations = {
 	.llseek		= ext4_dir_llseek,
 	.read		= generic_read_dir,
-	.readdir	= ext4_readdir,
+	.iterate	= ext4_readdir,
 	.unlocked_ioctl = ext4_ioctl,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl	= ext4_compat_ioctl,
--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1505,7 +1505,6 @@ int fiemap_check_flags(struct fiemap_extent_info *fieinfo, u32 fs_flags);
 typedef int (*filldir_t)(void *, const char *, int, loff_t, u64, unsigned);
 struct dir_context {
 	const filldir_t actor;
-	void *dirent;
 	loff_t pos;
 };
 
@@ -1513,7 +1512,7 @@ static inline bool dir_emit(struct dir_context *ctx,
 			    const char *name, int namelen,
 			    u64 ino, unsigned type)
 {
-	return ctx->actor(ctx->dirent, name, namelen, ctx->pos, ino, type) == 0;
+	return ctx->actor(ctx, name, namelen, ctx->pos, ino, type) == 0;
 }
 struct block_device_operations;
 
