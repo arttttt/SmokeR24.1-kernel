7525b862f7e50af0eeb4c90e50b6b196f6cd61e8
James Huang <jamehuang@nvidia.com>
2018-02-01 13:01:50 +0800

    arm64: define speculation barrier
    The instruction sequency "dsb sy" followed by "isb" functions as
    a speculation barrier, which prevents the instructions after that
    from being speculatively executed.
    
    bug 2039126
    
    Change-Id: Ie3b7b873a12002617e60510ed8759bdaa7cd7057
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1618222
    Signed-off-by: James Huang <jamehuang@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1650093
    Signed-off-by: Jeetesh Burman <jburman@nvidia.com>
    (cherry picked from commit f125c60045878513902cac4a084fde9a516eb3e2)
    Reviewed-on: https://git-master.nvidia.com/r/1660782
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>

--- arch/arm64/include/asm/barrier.h
+++ arch/arm64/include/asm/barrier.h
@@ -99,6 +99,10 @@ do {									\
 #define set_mb(var, value)	do { var = value; smp_mb(); } while (0)
 #define nop()		asm volatile("nop");
 
+#define speculation_barrier()                                           \
+        asm volatile(   "dsb sy\n"                                      \
+	                "isb\n" : : : "memory")
+
 #endif	/* __ASSEMBLY__ */
 
 #endif	/* __ASM_BARRIER_H */
