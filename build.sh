4ca6d87a55e67170c87d2fcf5ce80b0ae168187d
arttttt <artem-bambalov@yandex.ru>
2018-07-29 16:08:34 +0300

    build: updates
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- build.sh
+++ build.sh
@@ -8,10 +8,9 @@ clean_build=0
 config="tegra12_defconfig"
 dtb_name="tegra124-mocha.dtb"
 dtb_only=0
-kernel_name="SmokeR24.1"
 build_log="build.log"
 threads=5
-toolchain="$HOME/Android/toolchain/linaro-4.9.4/bin/arm-linux-gnueabihf-"
+toolchain="$HOME/PROJECTS/MIPAD/linaro-4.9.4/bin/arm-linux-gnueabihf-"
 
 KERNEL_DIR=$PWD
 ORIGINAL_OUTPUT_DIR="$KERNEL_DIR/arch/$ARCH/boot"
@@ -36,40 +35,17 @@ printfc() {
 	fi;
 }
 
-generate_version()
+make_img()
 {
-	if [[ -f "$KERNEL_DIR/.git/HEAD"  &&  -f "$KERNEL_DIR/anykernel/anykernel.sh" ]]; then
-		local updated_kernel_name
-		eval "$(awk -F"="  '/kernel.string/{print "anykernel_name="$2}' $KERNEL_DIR/anykernel/anykernel.sh)"
-		eval "$(awk -F"-"  '{print "current_branch="$2}' $KERNEL_DIR/.git/HEAD)"
-		if [[ "$current_branch" != "stable" ]]; then
-			if [[ ! -f "$KERNEL_DIR/version" ]]; then
-				echo "build_number=0" > $KERNEL_DIR/version
-			fi;
-
-			awk -F"="  '{$2+=1; print $1"="$2}' $KERNEL_DIR/version > tmpfile
-			mv tmpfile $KERNEL_DIR/version
-			eval "$(awk -F"="  '{print "current_build="$2}' $KERNEL_DIR/version)"
-			export LOCALVERSION="-build$current_build"
-			updated_kernel_name=$kernel_name"-"$current_branch"-build"$current_build
-		else
-			updated_kernel_name=$kernel_name"-"$current_branch
-		fi;
-			sed -i s/$anykernel_name/$updated_kernel_name/ $KERNEL_DIR/anykernel/anykernel.sh
-	fi;
-}
-
-make_zip()
-{
-	if [[ -d "$KERNEL_DIR/anykernel" ]]; then
-		printfc "\nСоздание zip архива\n\n" $HEAD
+	if [[ -d "$KERNEL_DIR/Initramfs" ]]; then
+		printfc "\nСоздание boot.img\n\n" $HEAD
 	else
-		printfc "\nПапка $KERNEL_DIR/anykernel не существует\n\n" $ERROR
+		printfc "\nПапка $KERNEL_DIR/Initramfs не существует\n\n" $ERROR
 		return
 	fi;
 
 	if [[ -f "$ORIGINAL_OUTPUT_DIR/zImage" ]]; then
-		mv $ORIGINAL_OUTPUT_DIR/zImage $PWD/anykernel/kernel/
+		mv $ORIGINAL_OUTPUT_DIR/zImage $PWD/Initramfs/
 	else
 		if [[ $dtb_only == 0 ]]; then
 			printfc "Файл $ORIGINAL_OUTPUT_DIR/zImage не существует\n\n" $ERROR
@@ -78,7 +54,7 @@ make_zip()
 	fi
 
 	if [[ -f "$ORIGINAL_OUTPUT_DIR/dts/$dtb_name" ]]; then
-		mv $ORIGINAL_OUTPUT_DIR/dts/$dtb_name $PWD/anykernel/kernel/dtb
+		mv $ORIGINAL_OUTPUT_DIR/dts/$dtb_name $PWD/Initramfs/dtb
 	else
 		if [[ $dtb_only == 0 ]]; then
 			printfc "Файл $ORIGINAL_OUTPUT_DIR/dts/$dtb_name не существует\n\n" $ERROR
@@ -86,26 +62,10 @@ make_zip()
 		fi
 	fi
 
-	cd $KERNEL_DIR/anykernel
-	local zip_name="$kernel_name($(date +'%d.%m.%Y-%H:%M')).zip"
-	zip -r $zip_name *
+	cd $KERNEL_DIR/Initramfs
 
-	if [[ -f "$PWD/$zip_name" ]]; then
-		if [[ ! -d "$OUTPUT_DIR" ]]; then
-			mkdir $OUTPUT_DIR
-		fi;
+	./build.sh
 
-		printfc "\n$zip_name создан, перемещение в $OUTPUT_DIR" $HEAD
-		mv "$PWD/$zip_name" $OUTPUT_DIR
-
-		if [[ -f "$OUTPUT_DIR/$zip_name" ]]; then
-			echo
-			printfc "\nЗавершено\n" $HEAD
-		fi
-	else
-		printfc "\nНе удалось создать архив\n" $ERROR
-		return
-	fi
 	cd $KERNEL_DIR
 }
 
@@ -119,7 +79,6 @@ compile()
 		make mrproper
 	fi
 
-	generate_version
 	make $config
 	make -j$threads ARCH=$ARCH CROSS_COMPILE=$toolchain zImage
 
@@ -168,9 +127,8 @@ compile()
 	local end=$(date +%s)
 	local comp_time=$((end-start))
 	printf "\e[1;32m\nЯдро скомпилировано за %02d:%02d\n\e[0m" $((($comp_time/60)%60)) $(($comp_time%60))
-	printfc "Сборка номер $current_build в ветке $current_branch\n" $HEAD
 
-	make_zip
+	make_img
 }
 
 compile_dtb()
@@ -178,7 +136,6 @@ compile_dtb()
 	clear
 
 	dtb_only=1
-	generate_version
 	make $config
 	make -j$threads ARCH=$ARCH CROSS_COMPILE=$toolchain $dtb_name
 
@@ -189,7 +146,7 @@ compile_dtb()
 		return
 	fi
 
-	make_zip
+	make_img
 }
 
 main()
