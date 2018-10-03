b1e447a4dedabd7eb85429c57f4f57c97c140618
arttttt <artem-bambalov@yandex.ru>
2018-10-04 00:59:37 +0300

    input: touchscreen: synaptics: pass IRQF ONESHOT to request threaded irq
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_core.c
@@ -1855,7 +1855,7 @@ static int synaptics_rmi4_irq_enable(struct synaptics_rmi4_data *rmi4_data,
 		}
 
 		retval = request_threaded_irq(rmi4_data->irq, NULL,
-				synaptics_rmi4_irq, bdata->irq_flags,
+				synaptics_rmi4_irq, bdata->irq_flags | IRQF_ONESHOT,
 				PLATFORM_DRIVER_NAME, rmi4_data);
 		if (retval < 0) {
 			dev_err(rmi4_data->pdev->dev.parent,
