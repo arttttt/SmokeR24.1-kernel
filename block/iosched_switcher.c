a97895044e6151bbad3c0ef7345b553217de81be
arttttt <artem-bambalov@yandex.ru>
2018-11-02 00:39:54 +0300

    Revert "iosched_switcher: Allow multiple block devices to be used"
    This reverts commit 6538cb131e0e8fb3640ba0f5d8f3cef39fae4992.
    
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -21,28 +21,24 @@
 #define NOOP_IOSCHED "noop"
 #define RESTORE_DELAY_MS (10000)
 
-struct req_queue_data {
-	struct list_head list;
+struct iosched_conf {
+	struct delayed_work restore_prev;
 	struct request_queue *queue;
 	char prev_e[ELV_NAME_MAX];
 	bool using_noop;
 };
 
-static struct delayed_work restore_prev;
-static DEFINE_SPINLOCK(init_lock);
-static struct req_queue_data req_queues = {
-	.list = LIST_HEAD_INIT(req_queues.list),
-};
+static struct iosched_conf *config_g;
 
-static void change_elevator(struct req_queue_data *r, bool use_noop)
+static void change_elevator(struct iosched_conf *c, bool use_noop)
 {
-	struct request_queue *q = r->queue;
+	struct request_queue *q = c->queue;
 	char name[ELV_NAME_MAX];
 
-	if (r->using_noop == use_noop)
+	if (c->using_noop == use_noop)
 		return;
 
-	r->using_noop = use_noop;
+	c->using_noop = use_noop;
 
 	spin_lock_irq(q->queue_lock);
 	strcpy(name, q->elevator->type->elevator_name);
@@ -50,26 +46,19 @@ static void change_elevator(struct req_queue_data *r, bool use_noop)
 
 	if (use_noop) {
 		if (strcmp(name, NOOP_IOSCHED)) {
-			strcpy(r->prev_e, name);
+			strcpy(c->prev_e, name);
 			elevator_change(q, NOOP_IOSCHED);
 		}
 	} else {
 		if (!strcmp(name, NOOP_IOSCHED))
-			elevator_change(q, r->prev_e);
+			elevator_change(q, c->prev_e);
 	}
-}
-
-static void change_all_elevators(struct list_head *head, bool use_noop)
-{
-	struct req_queue_data *r;
-
-	list_for_each_entry(r, head, list)
-		change_elevator(r, use_noop);
 }
 
 static int fb_notifier_callback(struct notifier_block *nb,
 		unsigned long action, void *data)
 {
+	struct iosched_conf *c = config_g;
 	struct fb_event *evdata = data;
 	int *blank = evdata->data;
 
@@ -83,7 +72,7 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * Switch back from noop to the original iosched after a delay
 		 * when the screen is turned on.
 		 */
-		schedule_delayed_work(&restore_prev,
+		schedule_delayed_work(&c->restore_prev,
 				msecs_to_jiffies(RESTORE_DELAY_MS));
 		break;
 	default:
@@ -92,8 +81,8 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * the fb notifier chain call in case weird things can happen
 		 * when switching elevators while the screen is off.
 		 */
-		cancel_delayed_work_sync(&restore_prev);
-		change_all_elevators(&req_queues.list, true);
+		cancel_delayed_work_sync(&c->restore_prev);
+		change_elevator(c, true);
 	}
 
 	return NOTIFY_OK;
@@ -105,31 +94,36 @@ static struct notifier_block fb_notifier_callback_nb = {
 
 static void restore_prev_fn(struct work_struct *work)
 {
-	change_all_elevators(&req_queues.list, false);
+	struct iosched_conf *c = container_of(work, typeof(*c),
+						restore_prev.work);
+
+	change_elevator(c, false);
 }
 
 int init_iosched_switcher(struct request_queue *q)
 {
-	struct req_queue_data *r;
+	struct iosched_conf *c;
 
-	r = kzalloc(sizeof(*r), GFP_KERNEL);
-	if (!r)
+	if (!q) {
+		pr_err("Request queue is NULL!\n");
+		return -EINVAL;
+	}
+
+	if (config_g) {
+		pr_err("Already registered a request queue!\n");
+		return -EINVAL;
+	}
+
+	c = kzalloc(sizeof(*c), GFP_KERNEL);
+	if (!c)
 		return -ENOMEM;
 
-	r->queue = q;
+	c->queue = q;
 
-	spin_lock(&init_lock);
-	list_add(&r->list, &req_queues.list);
-	spin_unlock(&init_lock);
+	config_g = c;
 
-	return 0;
-}
-
-static int iosched_switcher_core_init(void)
-{
-	INIT_DELAYED_WORK(&restore_prev, restore_prev_fn);
+	INIT_DELAYED_WORK(&c->restore_prev, restore_prev_fn);
 	fb_register_client(&fb_notifier_callback_nb);
 
 	return 0;
 }
-late_initcall(iosched_switcher_core_init);
