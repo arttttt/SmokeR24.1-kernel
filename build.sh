18d75a8d841239ad7abba3d79a87bd8673080c35
arttttt <artem-bambalov@yandex.ru>
2019-11-09 00:02:49 +0300

    build script: mkdir anykernel/kernel if it does not exist

--- build.sh
+++ build.sh
@@ -72,6 +72,9 @@ function make_zip()
 	fi;
 
 	if [[ -f "$ORIGINAL_OUTPUT_DIR/zImage" ]]; then
+		if [[ ! -d "$PWD/anykernel/kernel/" ]]; then
+			mkdir $PWD/anykernel/kernel/
+		fi;
 		mv $ORIGINAL_OUTPUT_DIR/zImage $PWD/anykernel/kernel/
 	else
 		if [[ $dtb_only == 0 ]]; then
