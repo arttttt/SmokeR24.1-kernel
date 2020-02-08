e3d4af359313319a5d03fddb967a2972e6b4c2ce
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-08 00:00:48 -0800

    [PATCH] simple_lmk: Simplify tricks used to speed up the death process
    set_user_nice() doesn't schedule, and although set_cpus_allowed_ptr()
    can schedule, it will only do so when the specified task cannot run on
    the new set of allowed CPUs. Since cpu_all_mask is used,
    set_cpus_allowed_ptr() will never schedule. Therefore, both the priority
    elevation and cpus_allowed change can be moved to inside the task lock
    to simplify and speed things up.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -185,6 +185,7 @@ static void scan_and_kill(unsigned long pages_needed)
 	/* Kill the victims */
 	atomic_set_release(&victims_to_kill, nr_to_kill);
 	for (i = 0; i < nr_to_kill; i++) {
+		static const struct sched_param sched_zero_prio;
 		struct victim_info *victim = &victims[i];
 		struct task_struct *vtsk = victim->tsk;
 
@@ -195,23 +196,14 @@ static void scan_and_kill(unsigned long pages_needed)
 		/* Accelerate the victim's death by forcing the kill signal */
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);
 
-		/* Grab a reference to the victim for later before unlocking */
-		get_task_struct(vtsk);
-		task_unlock(vtsk);
-	}
+		/* Elevate the victim to SCHED_RR with zero RT priority */
+		sched_setscheduler_nocheck(vtsk, SCHED_RR, &sched_zero_prio);
 
-	/* Try to speed up the death process now that we can schedule again */
-	for (i = 0; i < nr_to_kill; i++) {
-		struct task_struct *vtsk = victims[i].tsk;
-
-		/* Increase the victim's priority to make it die faster */
-		set_user_nice(vtsk, MIN_NICE);
-
-		/* Allow the victim to run on any CPU */
+		/* Allow the victim to run on any CPU. This won't schedule. */
 		set_cpus_allowed_ptr(vtsk, cpu_all_mask);
 
-		/* Finally release the victim reference acquired earlier */
-		put_task_struct(vtsk);
+		/* Finally release the victim's task lock acquired earlier */
+		task_unlock(vtsk);
 	}
 
 	/* Wait until all the victims die */
